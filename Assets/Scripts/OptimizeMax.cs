8ca23ad516a81aabb150d36be63286c7f068087b
Akkharadet Saengket <Aukkaradat.sang@bumail.net>
2025-08-09 12:55:09 +0700

    downtexture and script optimize

---
+++ Assets/Scripts/OptimizeMax.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OptimizeMax : MonoBehaviour
+{
+    private Renderer objRenderer;
+    private Camera mainCam;
+
+    void Start()
+    {
+        objRenderer = GetComponent<Renderer>();
+        mainCam = Camera.main;
+    }
+
+    void Update()
+    {
+        if (objRenderer == null || mainCam == null) return;
+
+        // ถ้า renderer มองไม่เห็นจากกล้อง
+        bool isVisible = GeometryUtility.TestPlanesAABB(
+            GeometryUtility.CalculateFrustumPlanes(mainCam),
+            objRenderer.bounds
+        );
+
+        gameObject.SetActive(isVisible);
+    }
+}
